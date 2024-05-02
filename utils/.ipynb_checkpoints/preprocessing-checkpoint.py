0bf72bcfa94e1df7558d30b53f165accb5107ff8
mustaphouni04 <elaichounimustapha@gmail.com>
2024-05-02 09:57:53 +0200

    making preprocessing verbose

--- utils/.ipynb_checkpoints/preprocessing-checkpoint.py
+++ utils/.ipynb_checkpoints/preprocessing-checkpoint.py
@@ -22,24 +22,38 @@ def final_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
     df_copy3 = df.copy()
     
     # Numerical features, one hot, normalize...
+    print("Dropping all unnecessary features...")
     df = preprocessing_CLEAN(df)
+    print("Done.")
+    print("Preprocessing timestamp...")
     df = preprocessing_TS(df)
+    print("Done.")
+    print("Preprocessing status_code...")
     df = preprocessing_SC(df)
+    print("Done.")
+    print("Preprocessing request_method...")
     df = preprocessing_RM(df)
+    print("Done.")
+    print("Preprocessing bytes_sent...")
     df = preprocessing_BS(df)
+    print("Done.")
 
     # Using the copies to get the correspondant embeddings for textual features
+    print("Preprocessing referer...")
     df_copy = preprocessing_R(df_copy)
     referer_column = df_copy['referer']
     df['referer'] = referer_column
-
+    print("Done.")
+    print("Preprocessing requested_url...")
     df_copy2 = preprocessing_RU(df_copy2)
     requested_url_column = df_copy2['requested_url']
     df['requested_url'] = requested_url_column
-
+    print("Done.")
+    print("Preprocessing user_agent...")
     df_copy3 = preprocessing_UA(df_copy3)
     user_agent_column = df_copy3['user_agent']
     df['user_agent'] = user_agent_column
+    print("Done.")
 
     return df
     
